969b914f6fc00f161ad598937b2190736eb39868
Kristina Iermolenko <kristina.iermolenko@points.com>
2025-09-11 13:09:52 -0600

    88 - merge sorted arrays

--- problems/88-merge-sorted-array.go
+++ problems/88-merge-sorted-array.go
@@ -24,23 +24,19 @@ func Merge(nums1 []int, m int, nums2 []int, n int) []int { // original does not 
 	lei := m + n - 1 // last element index
 	fmt.Fprintln(os.Stdout, lei)
 
-	for lei >= 0 {
+	for p2 >= 0 && lei >= 0 {
 		fmt.Fprintln(os.Stdout, "p1,p2,lei", p1, p2, lei)
 		fmt.Fprintln(os.Stdout, nums1[p1], nums2[p2])
 
 		fmt.Fprintln(os.Stdout, "----")
-		if nums1[p1] > nums2[p2] {
+		if p1 >= 0 && nums1[p1] > nums2[p2] {
 			nums1[lei], nums1[p1] = nums1[p1], nums1[lei]
-
 			if p1 > 0 {
 				p1--
 			}
 		} else {
 			nums1[lei] = nums2[p2]
-			if p2 > 0 {
-				p2--
-			}
-
+			p2--
 		}
 		lei--
 		fmt.Fprintln(os.Stdout, "nums1", nums1)
