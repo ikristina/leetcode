2d9f688c569db6acede2504592e7b798a0e7c5ac
Kristina Iermolenko <kristina.iermolenko@points.com>
2025-09-12 09:50:16 -0600

    88 - merge sorted arrays

--- problems/88-merge-sorted-array.go
+++ problems/88-merge-sorted-array.go
@@ -1,49 +1,31 @@
 package problems
 
-import (
-	"fmt"
-	"os"
-)
-
 func Merge(nums1 []int, m int, nums2 []int, n int) []int { // original does not have a return and replaces nums1 in place.
 	if n == 0 {
 		return nums1
 	}
-	fmt.Fprintln(os.Stdout, nums1, len(nums1))
-	fmt.Fprintln(os.Stdout, "========")
 
 	// ~ two pointers problem.
-	var p1 int
-	if m > 0 {
-		p1 = m - 1
-	} else {
-		p1 = 0
-	}
+
+	p1 := m - 1
 	p2 := n - 1
-
 	lei := m + n - 1 // last element index
-	fmt.Fprintln(os.Stdout, lei)
 
-	for p2 >= 0 && lei >= 0 {
-		fmt.Fprintln(os.Stdout, "p1,p2,lei", p1, p2, lei)
-		fmt.Fprintln(os.Stdout, nums1[p1], nums2[p2])
+	for p2 >= 0 {
 
-		fmt.Fprintln(os.Stdout, "----")
-		if p1 >= 0 && nums1[p1] > nums2[p2] {
-			nums1[lei], nums1[p1] = nums1[p1], nums1[lei]
-			if p1 > 0 {
-				p1--
-			}
+		if p1 >= 0 && nums1[p1] >= nums2[p2] {
+			nums1[lei] = nums1[p1]
+			p1--
 		} else {
 			nums1[lei] = nums2[p2]
 			p2--
 		}
 		lei--
-		fmt.Fprintln(os.Stdout, "nums1", nums1)
-
+		if lei < 0 {
+			break
+		}
 	}
 
-	fmt.Fprintln(os.Stdout, nums1, len(nums1))
 	return nums1
 
 }
--- problems/88-merge-sorted-array_test.go
+++ problems/88-merge-sorted-array_test.go
@@ -37,6 +37,14 @@ func TestMergeTC4(t *testing.T) {
 	}
 }
 
+func TestMergeTC5(t *testing.T) {
+	got := Merge([]int{0, 0, 3, 0, 0, 0, 0, 0, 0}, 3, []int{-1, 1, 1, 1, 2, 3}, 6)
+	want := []int{-1, 0, 0, 1, 1, 1, 2, 3, 3}
+	if !slices.Equal(got, want) {
+		t.Fatalf("Got: %v, Want: %v", got, want)
+	}
+}
+
 func BenchmarkMerge(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		_ = Merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
