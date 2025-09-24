b5d192a12ee93d9cfeb8be35d42d694ee3197c06
kristina <ikristina@tuta.io>
2025-09-23 19:35:45 -0600

    feat: trie code

---
+++ data_structures/trie.go
@@ -0,0 +1,54 @@
+package datastructures
+
+// Trie ...
+type Trie struct{
+	Root *TrieNode
+}
+
+// TrieNode ...
+type TrieNode struct{
+	Children map[rune]*TrieNode
+	IsEnd bool
+}
+
+// NewTrie ...
+func NewTrie() *Trie{
+	return &Trie{Root: &TrieNode{Children: make(map[rune]*TrieNode)}}
+}
+
+// Insert ...
+func (t *Trie) Insert(word string){
+	node := t.Root
+	for _, c := range word{
+		if _, ok := node.Children[c]; !ok{
+			node.Children[c] = &TrieNode{Children: make(map[rune]*TrieNode)}
+		}
+		node = node.Children[c]
+	}
+	node.IsEnd = true
+}
+
+// Search
+func (t *Trie) Search(word string) bool{
+	node := t.Root
+	for _, c := range word{
+		if _, ok := node.Children[c]; !ok{
+			return false
+		}
+		node = node.Children[c]
+	}
+	return node.IsEnd
+}
+
+// StartsWith
+func (t *Trie) StartsWith(prefix string) bool{
+	node := t.Root
+	for _, c := range prefix{
+		if _, ok := node.Children[c]; !ok{
+			return false
+		}
+		node = node.Children[c]
+	}
+	return true
+}
+
