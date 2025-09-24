4ccef876441e069c7aa4d5b590168e4a91d2c18c
kristina <ikristina@tuta.io>
2025-09-23 19:34:38 -0600

    test: trie tests

---
+++ data_structures/trie_test.go
@@ -0,0 +1,26 @@
+package datastructures
+
+
+func TestTrieInsert(t *testing.T) {
+	trie := NewTrie()
+	trie.Insert("hello")
+	trie.Insert("world")
+	trie.Insert("hi")
+	trie.Insert("hola")
+
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"hello", true},
+		{"world", true},
+		{"hi", true},
+		{"hola", true},
+	}
+
+	for _, test := range tests {
+		if trie.Search(test.input) != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, trie.Search(test.input))
+		}
+	}
+}
