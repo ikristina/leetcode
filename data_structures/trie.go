2b3f1e9eb4ed7a19bf141ad17187d8998e368a9a
kristina <ikristina@tuta.io>
2025-09-23 22:06:50 -0600

    chore: go mod tidy && comment out some code

--- data_structures/trie.go
+++ data_structures/trie.go
@@ -1,26 +1,26 @@
 package datastructures
 
 // Trie ...
-type Trie struct{
+type Trie struct {
 	Root *TrieNode
 }
 
 // TrieNode ...
-type TrieNode struct{
+type TrieNode struct {
 	Children map[rune]*TrieNode
-	IsEnd bool
+	IsEnd    bool
 }
 
 // NewTrie ...
-func NewTrie() *Trie{
+func NewTrie() *Trie {
 	return &Trie{Root: &TrieNode{Children: make(map[rune]*TrieNode)}}
 }
 
 // Insert ...
-func (t *Trie) Insert(word string){
+func (t *Trie) Insert(word string) {
 	node := t.Root
-	for _, c := range word{
-		if _, ok := node.Children[c]; !ok{
+	for _, c := range word {
+		if _, ok := node.Children[c]; !ok {
 			node.Children[c] = &TrieNode{Children: make(map[rune]*TrieNode)}
 		}
 		node = node.Children[c]
@@ -28,11 +28,11 @@ func (t *Trie) Insert(word string){
 	node.IsEnd = true
 }
 
-// Search
-func (t *Trie) Search(word string) bool{
+// Search ...
+func (t *Trie) Search(word string) bool {
 	node := t.Root
-	for _, c := range word{
-		if _, ok := node.Children[c]; !ok{
+	for _, c := range word {
+		if _, ok := node.Children[c]; !ok {
 			return false
 		}
 		node = node.Children[c]
@@ -40,15 +40,14 @@ func (t *Trie) Search(word string) bool{
 	return node.IsEnd
 }
 
-// StartsWith
-func (t *Trie) StartsWith(prefix string) bool{
+// StartsWith ...
+func (t *Trie) StartsWith(prefix string) bool {
 	node := t.Root
-	for _, c := range prefix{
-		if _, ok := node.Children[c]; !ok{
+	for _, c := range prefix {
+		if _, ok := node.Children[c]; !ok {
 			return false
 		}
 		node = node.Children[c]
 	}
 	return true
 }
-
