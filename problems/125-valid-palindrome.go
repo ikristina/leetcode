4be193a656d2016b4a95a85c05db690987620766
kristina <ikristina@tuta.io>
2026-03-03 17:27:28 -0700

    125-valid-palindrome option 2

--- problems/125-valid-palindrome.go
+++ problems/125-valid-palindrome.go
@@ -3,27 +3,52 @@ package problems
 import "strings"
 
 // A map[byte]struct{} uses less memory than map[byte]bool because struct{} has zero size.
-var valids = map[byte]struct{}{
-	'a': {}, 'b': {}, 'c': {}, 'd': {}, 'e': {}, 'f': {}, 'g': {}, 'h': {}, 'i': {}, 'j': {}, 'k': {}, 'l': {}, 'm': {}, 'n': {}, 'o': {}, 'p': {}, 'q': {}, 'r': {}, 's': {}, 't': {}, 'u': {}, 'v': {}, 'w': {}, 'x': {}, 'y': {}, 'z': {},
-	'0': {}, '1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {},
-}
+//var valids = map[byte]struct{}{
+//	'a': {}, 'b': {}, 'c': {}, 'd': {}, 'e': {}, 'f': {}, 'g': {}, 'h': {}, 'i': {}, 'j': {}, 'k': {}, 'l': {}, 'm': {}, 'n': {}, 'o': {}, 'p': {}, 'q': {}, 'r': {}, 's': {}, 't': {}, 'u': {}, 'v': {}, 'w': {}, 'x': {}, 'y': {}, 'z': {},
+//	'0': {}, '1': {}, '2': {}, '3': {}, '4': {}, '5': {}, '6': {}, '7': {}, '8': {}, '9': {},
+//}
+//
+//func isPalindrome(s string) bool {
+//	if len(s) == 0 || len(s) == 1 {
+//		return true
+//	}
+//	s = strings.ToLower(s) // O(N)
+//	start := 0
+//	end := len(s) - 1
+//	for start < end {
+//		if _, ok := valids[s[start]]; !ok {
+//			start++
+//			continue
+//		}
+//		if _, ok := valids[s[end]]; !ok {
+//			end--
+//			continue
+//		}
+//		if s[start] != s[end] {
+//			return false
+//		}
+//		start++
+//		end--
+//	}
+//	return true
+//}
 
+// option 2
 func isPalindrome(s string) bool {
-	if len(s) == 0 || len(s) == 1 {
-		return true
-	}
-	s = strings.ToLower(s) // O(N)
-	start := 0
-	end := len(s) - 1
+	s = strings.ToLower(s)
+	start, end := 0, len(s)-1
 	for start < end {
-		if _, ok := valids[s[start]]; !ok {
+
+		if !isAlphaNumeric(s[start]) {
 			start++
 			continue
 		}
-		if _, ok := valids[s[end]]; !ok {
+
+		if !isAlphaNumeric(s[end]) {
 			end--
 			continue
 		}
+
 		if s[start] != s[end] {
 			return false
 		}
@@ -31,4 +56,10 @@ func isPalindrome(s string) bool {
 		end--
 	}
 	return true
+}
+func isAlphaNumeric(c byte) bool {
+	if (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z') {
+		return true
+	}
+	return false
 }
